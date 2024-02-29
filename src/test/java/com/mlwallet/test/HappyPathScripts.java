59ca772f4a45f3d9c6a46de4b3e2422b9dfb4e7d
FrancisMercado <mlqaautomation@mlhuillier.com>
2024-02-29 09:47:55 +0800

    HappyPath Class

---
+++ src/test/java/com/mlwallet/test/HappyPathScripts.java
@@ -0,0 +1,12 @@
+package com.mlwallet.test;
+
+import com.driverInstance.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class HappyPathScripts extends BaseTest {
+    @Test(priority = 18)
+    public void GC_SM_TC_76_Validate_Back_Home_Btn_In_GCash_View_Recent_Transaction_Dashboard_Page() throws Exception {
+        mlWalletGCash.GC_SM_TC_76_Validate_Back_Home_Btn_In_GCash_View_Recent_Transaction_Dashboard_Page();
+    }
+}
